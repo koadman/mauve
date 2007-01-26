5646d16a3af5c8475b1a4113a6bec0e52fa710ab
Aaron Darling <aaron.darling@uts.edu.au>
2007-01-26 02:15:18 +0000

    Added Anna to the developers list

--- src/org/gel/mauve/gui/Mauve.java
+++ src/org/gel/mauve/gui/Mauve.java
@@ -139,7 +139,7 @@ public class Mauve
             ioe.printStackTrace(MyConsole.err());
         }
 
-        about_message = "<html><center>Mauve version " + release_version + " build " + build_number + " (c) 2003-2005 Aaron Darling and Paul Infield-Harm<br>" + "<a href=\"http://gel.ahabs.wisc.edu\">http://gel.ahabs.wisc.edu/mauve</a><br>" + "<p>Mauve is free software.  See COPYING for details.</center></p><p>CITATION:<br>Mauve: Multiple Alignment of Conserved Genomic Sequence With Rearrangements.<br>Aaron C. E. Darling, Bob Mau, Frederick R. Blattner, Nicole T. Perna.<br><i>Genome Research</i> <b>14</b>(7):1394-1403</p><p>Mauve uses the Cinema-MX alignment editing modules written by Phillip Lord<br>DCJ rearrangement history inference contributed by Mike Tsai</p>";
+        about_message = "<html><center>Mauve version " + release_version + " build " + build_number + " (c) 2003-2007 Aaron Darling, Paul Infield-Harm, and Anna Rissman<br>" + "<a href=\"http://gel.ahabs.wisc.edu\">http://gel.ahabs.wisc.edu/mauve</a><br>" + "<p>Mauve is free, open-source software.  See COPYING for details.</center></p><p>CITATION:<br>Mauve: Multiple Alignment of Conserved Genomic Sequence With Rearrangements.<br>Aaron C. E. Darling, Bob Mau, Frederick R. Blattner, Nicole T. Perna.<br><i>Genome Research</i> <b>14</b>(7):1394-1403</p><p>Mauve uses the Cinema-MX alignment editing modules written by Phillip Lord<br>DCJ rearrangement history inference contributed by Mike Tsai</p>";
 
         checkForUpdates();
 
