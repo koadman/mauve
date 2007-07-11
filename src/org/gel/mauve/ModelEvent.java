0cf9e3727616483dd5a1f2904c19278bf3d29b9a
Aaron Darling <aaron.darling@uts.edu.au>
2007-07-11 04:19:40 +0000

    added a serialVersionUID.  i don't understand these things.

--- src/org/gel/mauve/ModelEvent.java
+++ src/org/gel/mauve/ModelEvent.java
@@ -5,6 +5,7 @@ import java.util.EventObject;
 
 public class ModelEvent extends EventObject
 {
+	static final long serialVersionUID = 234234;
     public ModelEvent(BaseViewerModel source)
     {
         super(source);
