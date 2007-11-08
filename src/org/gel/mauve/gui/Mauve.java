8442ef74bca3e2f2ec5d222c2789a9bfecc2e678
Anna Rissman <rissman@wisc.edu>
2007-11-08 22:47:23 +0000

    Applying ASAP goTo feature to release branch

--- src/org/gel/mauve/gui/Mauve.java
+++ src/org/gel/mauve/gui/Mauve.java
@@ -29,116 +29,91 @@ import javax.swing.SwingUtilities;
 import org.gel.mauve.MyConsole;
 import org.gel.mauve.remote.RemoteControlImpl;
 
-public class Mauve
-{
-    static Properties props = new Properties();
-    static String about_message = "";
+public class Mauve {
+	static Properties props = new Properties ();
+	static String about_message = "";
 
-    private MauveFrame availableFrame;
-    private Vector frames; // List of open frames
+	protected MauveFrame availableFrame;
+	protected Vector frames; // List of open frames
+	protected boolean check_updates = true;
 
-    private Mauve()
-    {
-    }
+	protected Mauve () {
+	}
 
-    // Main entry point
-    static public void main(String[] args)
-    {
-        if (args.length == 1)
-        {
-            final String filename = args[0];
-            javax.swing.SwingUtilities.invokeLater(new Runnable()
-                    {
-                        public void run()
-                        {
-                            Mauve m = new Mauve();
-                            m.init(filename);
-                        }
-                    });
-        }
-        else
-        {
-            javax.swing.SwingUtilities.invokeLater(new Runnable()
-                    {
-                        public void run()
-                        {
-                            Mauve m = new Mauve();
-                            m.init();
-                        }
-                    });
-        }
-    }
+	// Main entry point
+	static public void main (String [] args) {
+		mainHook (args, new Mauve ());
+	}
+	
+	public static void mainHook (String args [], final Mauve mv) {
+		if (args.length >= 1) {
+			final String filename = args[0];
+			javax.swing.SwingUtilities.invokeLater (new Runnable () {
+				public void run () {
+					mv.init (filename);
+				}
+			});
+		} else {
+			javax.swing.SwingUtilities.invokeLater (new Runnable () {
+				public void run () {
+					mv.init ();
+				}
+			});
+		}
+	}
 
-    
-    public void init(String filename)
-    {
-        init();
-        if( filename.indexOf("://") == -1 ){
-        	loadFile(new File(filename));
-        }else	// this looks like a URL, try to load it
-        	try{
-        		loadURL(new URL(filename));
-        	}catch(MalformedURLException mue){
-        		mue.printStackTrace();
-        	}
+	public void init (String filename) {
+		init ();
+		if (filename.indexOf ("://") == -1) {
+			loadFile (new File (filename));
+		} else
+			// this looks like a URL, try to load it
+			try {
+				loadURL (new URL (filename));
+			} catch (MalformedURLException mue) {
+				mue.printStackTrace ();
+			}
     }
     
     private String release_version = "unknown";
     private String build_number = "0";
+	
+	public synchronized void init () {
+		// On OS X the aqua look and feel is default, but we can't develop for
+		// so
+		// many different looks and feels. Set it to Metal.
+		try {
+			javax.swing.UIManager
+					.setLookAndFeel ("javax.swing.plaf.metal.MetalLookAndFeel");
+		} catch (InstantiationException e) {
+		} catch (ClassNotFoundException e) {
+		} catch (javax.swing.UnsupportedLookAndFeelException e) {
+		} catch (IllegalAccessException e) {
+		}
 
-    public synchronized void init()
-    {
-        // On OS X the aqua look and feel is default, but we can't develop for
-        // so
-        // many different looks and feels. Set it to Metal.
-        try
-        {
-            javax.swing.UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        }
-        catch (InstantiationException e)
-        {
-        }
-        catch (ClassNotFoundException e)
-        {
-        }
-        catch (javax.swing.UnsupportedLookAndFeelException e)
-        {
-        }
-        catch (IllegalAccessException e)
-        {
-        }
+		if (!hasRequiredJVM ())
+			return;
+		if (!System.getProperties ().containsKey ("mauve.force.console")) {
+			MyConsole.setUseSwing (true);
+		}
+		if (System.getProperties ().containsKey ("mauve.enable.remote")) {
+			RemoteControlImpl.startRemote (this);
+		}
 
-        if (!hasRequiredJVM())
-            return;
-        
-        
-        if (!System.getProperties().containsKey("mauve.force.console"))
-        {
-            MyConsole.setUseSwing(true);
-        }
-        
-        if (System.getProperties().containsKey("mauve.enable.remote"))
-        {
-            RemoteControlImpl.startRemote(this);
-        }
+		// read in the version properties file
+		try {
+			InputStream props_stream = MauveFrame.class
+					.getResourceAsStream ("/version.properties");
 
-        // read in the version properties file
-        try
-        {
-            InputStream props_stream = MauveFrame.class.getResourceAsStream("/version.properties");
-
-            if (props_stream != null)
-            {
-                props.load(props_stream);
-                release_version = props.getProperty("release.version");
-                build_number = props.getProperty("build.number");
-            }
-        }
-        catch (IOException ioe)
-        {
-            MyConsole.err().println("Couldn't read version.properties file.");
-            ioe.printStackTrace(MyConsole.err());
-        }
+			if (props_stream != null) {
+				props.load (props_stream);
+				release_version = props.getProperty ("release.version");
+				build_number = props.getProperty ("build.number");
+			}
+		} catch (IOException ioe) {
+			MyConsole.err ().println ("Couldn't read version.properties file.");
+			ioe.printStackTrace (MyConsole.err ());
+		}
 
         about_message = "<html><center>Mauve version " + release_version + " build " + build_number + " (c) 2003-2007 Aaron Darling, Paul Infield-Harm, and Anna Rissman<br>" + "<a href=\"http://gel.ahabs.wisc.edu\">http://gel.ahabs.wisc.edu/mauve</a><br>" + "<p>Mauve is free, open-source software.  See COPYING for details.</center></p><p>CITATION:<br>Mauve: Multiple Alignment of Conserved Genomic Sequence With Rearrangements.<br>Aaron C. E. Darling, Bob Mau, Frederick R. Blattner, Nicole T. Perna.<br><i>Genome Research</i> <b>14</b>(7):1394-1403</p><p>Mauve uses the Cinema-MX alignment editing modules written by Phillip Lord<br>DCJ rearrangement history inference contributed by Mike Tsai</p>";
 
@@ -151,50 +126,57 @@ public class Mauve
                     }
                 });
 
-        frames = new Vector();
-        availableFrame = new MauveFrame(this);
-        frames.add(availableFrame);
-        new SplashScreen("/images/mauve_logo.png", about_message, null, 3000);
-//        mauve_splash.dispose();
-//        mauve_splash.setVisible(false);
-    }
+		frames = new Vector ();
+		availableFrame = makeNewFrame ();
+		new SplashScreen ("/images/mauve_logo.png", about_message, null, 3000);
+		// mauve_splash.dispose();
+		// mauve_splash.setVisible(false);
+	}
 
-    /**
-     * Checks the Mauve web server for a newer version of this software
-     */
-    public void checkForUpdates()
-    {
-        try
-        {
-            String os_type = System.getProperty("os.name");
-            String build_date = props.getProperty("build.timestamp");
+	/**
+	 * Checks the Mauve web server for a newer version of this software
+	 */
+	public void checkForUpdates () {
+		if (!check_updates)
+			return;
+		try {
+			String os_type = System.getProperty ("os.name");
+			String build_date = props.getProperty ("build.timestamp");
 
-            if (build_date == null)
-            {
-                MyConsole.err().println("Couldn't check version, build.timestamp property not found.");
-                return;
-            }
-            long local_version = Long.parseLong(build_date);
+			if (build_date == null) {
+				MyConsole
+						.err ()
+						.println (
+								"Couldn't check version, build.timestamp property not found.");
+				return;
+			}
+			long local_version = Long.parseLong (build_date);
 
-            // default to checking the linux version since it's
-            // probably the least standardized OS currently
-            String os_suffix = "linux";
-            if (os_type.startsWith("Windows"))
-                os_suffix = "windows";
-            else if (os_type.startsWith("Mac"))
-                os_suffix = "mac";
-            URL hurl = new URL("http://gel.ahabs.wisc.edu/mauve/downloads/latest." + os_suffix);
-            BufferedReader latest_in = new BufferedReader(new InputStreamReader(hurl.openStream()));
-            long latest_version = Long.parseLong(latest_in.readLine());
+			// default to checking the linux version since it's
+			// probably the least standardized OS currently
+			String os_suffix = "linux";
+			if (os_type.startsWith ("Windows"))
+				os_suffix = "windows";
+			else if (os_type.startsWith ("Mac"))
+				os_suffix = "mac";
+			URL hurl = new URL (
+					"http://gel.ahabs.wisc.edu/mauve/downloads/latest."
+							+ os_suffix);
+			BufferedReader latest_in = new BufferedReader (
+					new InputStreamReader (hurl.openStream ()));
+			long latest_version = Long.parseLong (latest_in.readLine ());
 
-            if (local_version < latest_version)
-            {
-                // check the OS type since linux can't do an auto-update
-                if (!os_suffix.equals("windows"))
-                {
-                    JOptionPane.showMessageDialog(null, "<html>An updated version of Mauve is available.<br>See the Mauve web site at <a href=\"http://gel.ahabs.wisc.edu/mauve\">http://gel.ahabs.wisc.edu/mauve</a> for more details...", "Updated Mauve available", JOptionPane.INFORMATION_MESSAGE);
-                    return;
-                }
+			if (local_version < latest_version) {
+				// check the OS type since linux can't do an auto-update
+				if (!os_suffix.equals ("windows")) {
+					JOptionPane
+							.showMessageDialog (
+									null,
+									"<html>An updated version of Mauve is available.<br>See the Mauve web site at <a href=\"http://gel.ahabs.wisc.edu/mauve\">http://gel.ahabs.wisc.edu/mauve</a> for more details...",
+									"Updated Mauve available",
+									JOptionPane.INFORMATION_MESSAGE);
+					return;
+				}
 
                 int dl_val = JOptionPane.showConfirmDialog(null, "An updated version of Mauve is available.\nWould you like to download and install it?", "Updated Mauve available", JOptionPane.YES_NO_OPTION);
                 if (dl_val == 0)
@@ -238,91 +220,81 @@ public class Mauve
         }
     }
 
-    private boolean hasRequiredJVM()
-    {
-        String jvm_version = System.getProperty("java.version");
-        int minor_version = Integer.parseInt(jvm_version.substring(2, 3));
-        if (jvm_version.charAt(0) == '1')
-        {
-            if (minor_version < 4)
-            {
-                MyConsole.err().println("Sorry, Mauve requires at least Java version 1.4 to operate correctly");
-                return false;
-            }
-        }
-        return true;
-    }
+	private boolean hasRequiredJVM () {
+		String jvm_version = System.getProperty ("java.version");
+		int minor_version = Integer.parseInt (jvm_version.substring (2, 3));
+		if (jvm_version.charAt (0) == '1') {
+			if (minor_version < 4) {
+				MyConsole
+						.err ()
+						.println (
+								"Sorry, Mauve requires at least Java version 1.4 to operate correctly");
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public synchronized void setFocus(String alignID, String sequenceID, long start, long end, String auth_token)
-    {
-        URL alignURL;
-        try
-        {
-            alignURL = new URL(alignID);
-        }
-        catch (MalformedURLException e)
-        {
-            e.printStackTrace();
-            return;
-        }
+	public synchronized void setFocus (String alignID, String sequenceID,
+			long start, long end, String auth_token, String contig) {
+		URL alignURL;
+		try {
+			alignURL = new URL (alignID);
+		} catch (MalformedURLException e) {
+			e.printStackTrace ();
+			return;
+		}
 
-        Iterator it = frames.iterator();
-        while (it.hasNext())
-        {
-            MauveFrame frame = (MauveFrame) it.next();
-            if (frame.model != null && alignURL.equals(frame.model.getSourceURL()))
-            {
-                frame.model.setFocus(sequenceID, start, end);
-                frame.toFront();
-                return;
-            }
-        }
+		Iterator it = frames.iterator ();
+		while (it.hasNext ()) {
+			MauveFrame frame = (MauveFrame) it.next ();
+			if (frame.model != null
+					&& alignURL.equals (frame.model.getSourceURL ())) {
+				frame.model.setFocus (sequenceID, start, end, contig);
+				frame.toFront ();
+				return;
+			}
+		}
 
-        // Load model and then zoom it.
-        MauveFrame frame = getNewFrame();
-        Thread t = new Thread(new FrameLoader(frame, alignURL, sequenceID, start, end, auth_token));
-        t.start();
-        
-    }
+		// Load model and then zoom it.
+		MauveFrame frame = getNewFrame ();
+		Thread t = new Thread (new FrameLoader (frame, alignURL, sequenceID,
+				start, end, auth_token, contig));
+		t.start ();
 
+	}
 
-    /**
-     * Load a file into a RearrangementPanel. Create a new frame if this frame
-     * is already displaying a genome alignment
-     */
-    public void loadFile(File rr_file)
-    {
-        MauveFrame frame = getNewFrame();
-        Thread t = new Thread(new FrameLoader(frame, rr_file));
-        t.start();
-    }
+	/**
+	 * Load a file into a RearrangementPanel. Create a new frame if this frame
+	 * is already displaying a genome alignment
+	 */
+	public void loadFile (File rr_file) {
+		MauveFrame frame = getNewFrame ();
+		Thread t = new Thread (new FrameLoader (frame, rr_file));
+		t.start ();
+	}
 
-    /**
-     * Load a file into a RearrangementPanel. Create a new frame if this frame
-     * is already displaying a genome alignment
-     */
-    public void loadURL(URL url)
-    {
-        MauveFrame frame = getNewFrame();
-        Thread t = new Thread(new FrameLoader(frame, url));
-        t.start();
-    }
-    
-    synchronized public void closeFrame(MauveFrame frame)
-    {
-        if (frames.size() > 1 || frame.rrpanel == null)
-        {
-            frame.setVisible(false);
-            frames.remove(frame);
-            frame.dispose();
-        }
-        else
-        {
-            frame.reset();
-            availableFrame = frame;
-        }
-    }
-    synchronized public void frameClosed()
+	/**
+	 * Load a file into a RearrangementPanel. Create a new frame if this frame
+	 * is already displaying a genome alignment
+	 */
+	public void loadURL (URL url) {
+		MauveFrame frame = getNewFrame ();
+		Thread t = new Thread (new FrameLoader (frame, url));
+		t.start ();
+	}
+
+	synchronized public void closeFrame (MauveFrame frame) {
+		if (frames.size () > 1 || frame.rrpanel == null) {
+			frame.setVisible (false);
+			frames.remove (frame);
+			frame.dispose ();
+		} else {
+			frame.reset ();
+			availableFrame = frame;
+		}
+	}
+	    synchronized public void frameClosed()
     {
 	    if (frames.size() == 0)
 	    {
@@ -335,23 +307,24 @@ public class Mauve
                 });
 	    }
     }
-    
-    synchronized private MauveFrame getNewFrame()
-    {
-        MauveFrame frame;
-        if (availableFrame != null)
-        {
-            frame = availableFrame;
-            availableFrame = null;
-        }
-        else
-        {
-            frame = new MauveFrame(this);
-            frames.add(frame);
-        }
-        return frame;
-    }
-    /**
+
+	synchronized protected MauveFrame getNewFrame () {
+		MauveFrame frame;
+		if (availableFrame != null) {
+			frame = availableFrame;
+			availableFrame = null;
+		} else {
+			frame = makeNewFrame ();
+		}
+		return frame;
+	}
+	
+	protected MauveFrame makeNewFrame () {
+		MauveFrame frame = new MauveFrame (this);
+		frames.add (frame);
+		return frame;
+	}
+	 /**
      *  The string with the version number
      * @return The version number
      */
