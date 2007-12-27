b93778f524c310aee860844ca71894ea92f681e7
Anna Rissman <rissman@wisc.edu>
2007-12-27 03:27:15 +0000

    Added more utility methods

--- src/org/gel/air/util/IOUtils.java
+++ src/org/gel/air/util/IOUtils.java
@@ -1,7 +1,12 @@
 package org.gel.air.util;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
@@ -37,6 +42,46 @@ public class IOUtils {
 			}
 			dir.delete();
 		} catch (Exception e) {
+		}
+	}
+
+	public static void writeLongArray (DataOutputStream out, long [] data) {
+		try {
+			for (int i = 0; i < data.length; i++)
+				out.writeLong(data [i]);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static long [] readLongArray (DataInputStream in, int length) {
+		try {
+			long [] data = new long [length];
+			for (int i = 0; i < data.length; i++)
+				data [i] = in.readLong ();
+			return data;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static DataInputStream getDataInputStream (String file) {
+		try {
+			return new DataInputStream (new BufferedInputStream (new FileInputStream (file)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static DataOutputStream getDataOutputStream (String file) {
+		try {
+			return new DataOutputStream (new BufferedOutputStream (
+					new FileOutputStream (file)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 
