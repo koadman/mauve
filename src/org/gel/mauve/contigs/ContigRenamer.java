9f9003abb954dcc13ae6a9fc9fc0c29f34432808
Andrew Tritt <andrew.j.tritt@gmail.com>
2010-04-11 01:37:13 +0000

    Deprecated org.gel.mauve.contigs.ContigRenamer.java This does nothing.

--- src/org/gel/mauve/contigs/ContigRenamer.java
+++ src/org/gel/mauve/contigs/ContigRenamer.java
@@ -7,9 +7,16 @@ import org.biojava.bio.seq.Feature;
 import org.gel.mauve.Chromosome;
 import org.gel.mauve.MauveHelperFunctions;
 
-public class ContigRenamer extends ContigReorderer {
-	
-	public Hashtable names;
+/**
+ * This class does nothing. 
+ * 
+ * @author andrew
+ *
+ */
+@Deprecated
+public class ContigRenamer {
+/*	
+	private Hashtable names;
 	
 	public void process () {
 		names = new Hashtable ();
@@ -28,40 +35,13 @@ public class ContigRenamer extends ContigReorderer {
 	public String getContigName (Chromosome chrom) {
 		return chrom.getName ();
 	}
-	
-	/**
-	 * removes number and underscore from beginning of name.
-	 * 
-	 * @param chrom
-	 * @param format_length
-	 * @return
-	 */
-	public String getContigName (Chromosome chrom, int format_length) {
-		String add = chrom.getName ();
-		return add.substring (format_length + 1, add.length ());
-	}
-	/**
-	 * for renaming contigs to prepend their current order to their name.
-	 * 
-	 * @param chrom
-	 * @param number
-	 * @param format_length
-	 * @return
-	 */
-	public String getContigName (Chromosome chrom, int number, int format_length) {
-		String add = number++ + "_";
-		while (add.length () <= format_length)
-			add = "0" + add;
-		add = add + chrom.getName ();
-		names.put (chrom.getName (), add);
-		return add;
-	}
+
 
 	/**
 	 * @param args
-	 */
+	
 	public static void main (String [] args) {
 		new ContigRenamer ().init (args);
 	}
-
+*/
 }
